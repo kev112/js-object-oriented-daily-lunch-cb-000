73f4c7067e5c2573ac647905ce4980e6e4fa9c37
Tao <werewaswas@live.com>
2018-08-04 23:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,6 +63,10 @@ class Meal {
     store.meals.push(this);
   }
 
+  deliveries() {
+    return store.deliveries.filter(delivery => delivery.mealId === this.id);
+  }
+
   customers() {
     return this.deliveries().map(delivery => delivery.customer());
   }
